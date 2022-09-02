5d73b5ba7ecf4b6d2c14e8c0740d88ba4b743eb2
karanchadha10 <karan10chadha@gmail.com>
2022-09-02 20:19:00 +0530

    assets added

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StudentRoutingModule } from './student-routing.module';
+import { IndexComponent } from './index/index.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IndexComponent
+  ],
   imports: [
     CommonModule,
     StudentRoutingModule
